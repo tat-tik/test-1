3d197d4ef778bd536be424f5b150861ffa92950c
Tanya <an5227952@gmail.com>
2025-04-15 21:35:17 +0300

    test1

---
+++ src/gamerhealth.js
@@ -0,0 +1,9 @@
+  
+export function gamerHealth(gamer) {
+    let healthTotal = (gamer.health < 15) ? "critical": 
+    (gamer.health >= 15 && gamer.health <= 50) ? "wounded":
+    "healthy";            
+    return healthTotal;
+};
+
+       
